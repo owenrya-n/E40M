29e644203f6fb97f18db657d781f174f58a34171
Metastable1883 <owenryan1883@gmail.com>
2022-02-17 00:59:26 -0800

    Update Lab2bStarterCode.ino

--- Lab2bStarterCode.ino
+++ Lab2bStarterCode.ino
@@ -1,39 +1,46 @@
 /*Owen Ryan E40M LAB2B*/
+/*
+Trick  1: Hesitant Box: Finger pauses for one second before resetting switch if switch is flipped before finger returns to resting position
+Trick 2: Slow Box: Finger slowly returns to switch if switch is flipped before finger returns to resting position
+Box randomly switches between behaviors iva the random(); method
+*/
 void setup() {
   //start serial connection
   Serial.begin(9600);
-  //configure pin 2 as an input and enable the internal pull-up resistor
   pinMode(12, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP); //limitswitch
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
 }
 void loop() {
-  int speed=random(0,1000);
+  int choice=random(0,10);
   bool sl = false;
   Serial.println(sl);
+  Serial.println(choice);
   if(digitalRead(11)==LOW){//not at limit
-    if(digitalRead(12)==LOW){//off
+    if(digitalRead(12)==LOW){//off & backwards
       digitalWrite(2,HIGH);
       digitalWrite(3,LOW);
     }
-    if(digitalRead(12)==HIGH && sl==false){//on
+    if(digitalRead(12)==HIGH && sl==false){//on & forwards
+    if(choice >= 5){ //Hesitant box activated
       digitalWrite(2,LOW);
       digitalWrite(3,LOW);
-      delay(1000);
+      delay(1000);}
       digitalWrite(2,LOW);
-      digitalWrite(3,HIGH);
+      if(choice >= 5) digitalWrite(3,HIGH);
+      if(choice << 5) analogWrite(3, 110); //Slow Box Activated
       while(digitalRead(12)==HIGH){ //captive loop for rebound
         Serial.println("hold");
                 delay(10);
       }
     }
   }else if(digitalRead(11)==HIGH){//at limit
-    if(digitalRead(12)==LOW){//off
+    if(digitalRead(12)==LOW){//off & stopped
       digitalWrite(2,LOW);
       digitalWrite(3,LOW);
     }
-    if(digitalRead(12)==HIGH){//on
+    if(digitalRead(12)==HIGH){//on & forwards
       sl = true;
       digitalWrite(2,LOW);
       digitalWrite(3,HIGH);
@@ -45,4 +52,7 @@ void loop() {
     }
   }
 }
+ 
+ 
+ 
 
