6d3c052cc69e4b99ed1b5c48e13b4bce1860071a
Metastable1883 <owenryan1883@gmail.com>
2022-02-23 19:43:57 -0800

    LETS GOOOOOO

--- LAB_3B/Main/Display.ino
+++ LAB_3B/Main/Display.ino
@@ -9,22 +9,22 @@
 #define trigPin2 0
 const byte ANODE_PINS[8] = {6, 7, 8, 9, 10, 11, 12, 13};
 const byte CATHODE_PINS[8] = {A3, A2, A1, A0, 5, 4, 3, 2};
-const byte settings[7] = {5, 7, 9, 11, 13, 15, 17};
+const byte settings[8] = {5, 7, 9, 11, 13, 15, 17, 19};
 int d1;
 int d2;
-int clock = 200;
+int clock = 1000;
 int prev[2] = {4, 1};
-unsigned long timescale = 200UL;
+unsigned long timescale = 500UL;
 unsigned long freq;
 int multiplier = 1;
 int dx = 1;
 int dy = 1;
+int timef=0;
 
 void setup()
 {
     setPins();
     delay(2000);
-
 }
 
 void loop()
@@ -34,7 +34,7 @@ void loop()
     d2 = ping(echoPin1, trigPin1, 2);
     ramp(d1, 0, clock, settings);
     ramp(d2, 7, clock, settings);
-    if (millis() - freq >= timescale)
+    if (millis() - freq >= timescale-10*timef)
     {
         freq = millis();
         if (prev[0] >= 7 || prev[0] <= 0)
@@ -47,23 +47,29 @@ void loop()
         }
         prev[0] = iteratex(prev[0], prev[1], dx, dy, multiplier);
         prev[1] = iteratey(prev[0], prev[1], dx, dy, multiplier);
-        
     }
 
     if (contact(prev[0], prev[1], d1, d2, settings) == 3)
-        {
-           blink(0,0,100);
-           dx=0;
-           dy=0;
-        }
-        if (contact(prev[0], prev[1], d1, d2, settings) == 4)
-        {
-            blink(0,0,100);
-            dx=0;
-            dy=0;
-        }
+    {
+        goalanimation(3);
+        delay(1000);
+        prev[1]=rand() %2 +3;
+        prev[0]=rand() %7;
+        dx=-dx;
+        dy=-dy;
+        timef++;
+    }
+    if (contact(prev[0], prev[1], d1, d2, settings) == 4)
+    {
+        goalanimation(4);
+        delay(1000);
+        prev[1]=rand() %2 +3;
+        prev[0]=rand() %7;
+        dx=-dx;
+        dy=-dy;
+        timef++;
+    }
 
     display(prev, clock);
     delay(10);
-    //Serial.println(contact(prev[0], prev[1], d1, d2, settings));
 }
--- LAB_3B/Main/gamefunctions.h
+++ LAB_3B/Main/gamefunctions.h
@@ -3,9 +3,6 @@
 #include <Arduino.h>
 
 void showscore(int scorep1, int scorep2);
-void newgame();
-int trackgoal(int posx);
-void speedchange();
 int contact(int x, int y, int dstp1, int dstp2, const byte stsp[7]);
-const byte bounce(int x, int y, int xp, int yp, int p1, int p2, int t);
+void goalanimation(int player);
 #endif
